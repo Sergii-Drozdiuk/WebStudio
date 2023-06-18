58e79f9dd227eaf2bdcd18c1c12d5ecad37d317f
Sergii-Drozdiuk <drozduyk@gmail.com>
2023-06-18 21:22:37 +0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,44 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+const openModalBtn = document.querySelector("[data-modal-open]");
+const closeModalBtn = document.querySelector("[data-modal-close]");
+const modal = document.querySelector("[data-modal]");
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+  const isMenuOpen = openModalBtn.getAttribute('aria-expanded') === 'true' || false;
+  openModalBtn.setAttribute('aria-expanded', !isMenuOpen);
+
+  const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
+  bodyScrollLock[scrollLockMethod](document.body);
+
+  modal.classList.toggle("is-hidden");
   }
+
+  openModalBtn.addEventListener("click", toggleModal);
+  closeModalBtn.addEventListener("click", toggleModal);
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.add('is-hidden');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
 })();
+
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector("[data-modal-open]"),
+//     closeModalBtn: document.querySelector("[data-modal-close]"),
+//     modal: document.querySelector("[data-modal]"),
+//   };
+
+//   refs.openModalBtn.addEventListener("click", toggleModal);
+//   refs.closeModalBtn.addEventListener("click", toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle("is-hidden");
+//   }
+// })();
+
